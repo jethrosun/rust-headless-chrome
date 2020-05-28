091b1fdc33fa1d0e55c17d52c3bfbe89ad379b63
jethrosun <jethro.rules@real.com>
2020-05-28 19:38:46 -0400

    Extend the idle browser timeout to more thant 10 min

--- src/browser/process.rs
+++ src/browser/process.rs
@@ -99,7 +99,7 @@ pub struct LaunchOptions<'a> {
 
     /// How long to keep the WebSocket to the browser for after not receiving any events from it
     /// Defaults to 30 seconds
-    #[builder(default = "Duration::from_secs(300)")]
+    #[builder(default = "Duration::from_secs(800)")]
     pub idle_browser_timeout: Duration,
 
     /// Environment variables to set for the Chromium process.
